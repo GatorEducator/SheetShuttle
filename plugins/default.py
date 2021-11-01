f5ed8e16913b6b6cc7f2dffe6abe1d76006da080
Noor Buchi <buchin@allegheny.edu>
2021-11-01 00:33:41 -0400

    feat: begin work on CLI and plugin system

---
+++ plugins/default.py
@@ -0,0 +1,10 @@
+""""Default plugin for GridGopher."""
+
+# from gridgopher import sheet_collector
+# import os
+
+
+def run():
+    """Standard run function."""
+    # collector = sheet_collector.SheetCollector()
+    print("hello from the default plugin")
