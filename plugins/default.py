402f0cc641a3e2a3fea61dda28d606fbae50731a
Noor Buchi <buchin@allegheny.edu>
2022-01-14 13:44:09 -0500

    fix: remove github access token from plugin

--- plugins/default.py
+++ plugins/default.py
@@ -11,9 +11,3 @@ from github import Github
 def run(sheets_keys_file, sheets_config_directory, **kwargs):
     """Standard run function."""
     print("hello from the default plugin")
-    # print("Received Arguments: ")
-    # print(f"sheets_keys_file: {sheets_keys_file}")
-    # print(f"sheets_config_directory: {sheets_config_directory}")
-
-    # g = Github("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")
-    # repo = g.get_repo("AC-GopherBot/test-1")
