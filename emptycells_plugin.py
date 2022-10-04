9db6a6fea67bbed4e49ff9b1d8da72ec2521e840
David Gormley <gormley01@allegheny.edu>
2022-10-04 15:00:57 -0400

    adding empty plugin

---
+++ emptycells_plugin.py
@@ -0,0 +1,9 @@
+""""Standard empty plugin for SheetShuttle."""
+
+from sheetshuttle import github_objects
+from sheetshuttle import sheet_collector
+
+# This function is required
+def run(sheets_keys_file, sheets_config_directory, gh_config_directory, **kwargs):
+    """Standard run function."""
+    pass
