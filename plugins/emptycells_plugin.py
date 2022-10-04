2b898ef0e693bf2f32e20298878892805a81de9d
Aidan Neeson <neeson01@allegheny.edu>
2022-10-04 15:44:19 -0400

    Add code in plugin to access our sheet

--- plugins/emptycells_plugin.py
+++ plugins/emptycells_plugin.py
@@ -13,4 +13,7 @@ from github import Github
 # This function is required
 def run(sheets_keys_file, sheets_config_directory, gh_config_directory, **kwargs):
     """Standard run function."""
+    my_collector = sheet_collector.SheetCollector(sources_dir=sheets_config_directory)
+    my_collector.collect_files()
+    used_config = my_collector.sheets_data["git_creative_config"]
     pass
