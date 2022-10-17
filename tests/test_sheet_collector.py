99590bb5e8d9dab965c9ba24e8b030948d6a9135
Cole Kaupelis <kaupelisc@cslaptop.localdomain>
2022-10-17 19:55:38 -0400

    test case not working

--- tests/test_sheet_collector.py
+++ tests/test_sheet_collector.py
@@ -10,6 +10,7 @@ import yaml
 import pandas as pd
 
 from sheetshuttle import sheet_collector
+from sheetshuttle import util
 
 
 def test_region_initialize():
@@ -395,3 +396,13 @@ def test_sheet_collector_collect_files_prints_output(tmpdir, test_data, capfd):
     captured = capfd.readouterr()
     for print_item in test_data["collect_files_test"]["expected_print"]:
         assert print_item in captured.out
+
+def test_grab_returns_actual_url():
+    expected_id = "1XKnoa1BBzEnJ1TA_LTRs5e0zcva0SCgNyt7cfMVGHWc"
+    url_one = util.extract_sheet_id("https://docs.google.com/spreadsheets/d/1XKnoa1BBzEnJ1TA_LTRs5e0zcva0SCgNyt7cfMVGHWc/edit#gid=0")
+    assert url_one == expected_id
+
+def test_grab_returns_no_prefix():
+    expected_id = "my_very_important_key"
+    url_two = util.extract_sheet_id("docs.google.com/spreadsheets/d/my_very_important_key/edit#gid=0")
+    assert url_two == expected_id
