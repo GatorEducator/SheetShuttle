0fa19b07d554243cb475db08aa36311f26e1a2cc
Jeff Normile <normile01@allegheny.edu>
2022-10-18 08:03:04 -0400

    Fix final pylint issue (using built-in next)

--- sheetshuttle/util.py
+++ sheetshuttle/util.py
@@ -30,10 +30,14 @@ def extract_sheet_id(url: str) -> str:
     """Extract a sheet ID from a Google Sheets URL."""
     try:
         url_segments = url.split("/")
-        for i in range(len(url_segments)):
-            if url_segments[i] == "d":
-                return url_segments[i + 1]
-        return url
+        return next(
+            (
+                url_segments[i + 1]
+                for i in range(len(url_segments))
+                if url_segments[i] == "d"
+            ),
+            url,
+        )
     except IndexError:
         print("The URL provided is not valid for Google Sheets ID extraction.")
         return url
